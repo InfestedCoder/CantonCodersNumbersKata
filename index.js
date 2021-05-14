0f769e9395acfcb805601f6f3d8a16718ad0055d
Jeff Ginn <jeff.ginn@slalom.com>
2021-05-13 21:02:55 -0400

    Changes from May 13, 2021 Canton Coders session

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-export const answer = () => {
-    return 6 * 9;
+export const answer = (input) => {
+    const dict = {1:"one",2:"two",3:"three",11:"eleven"};
+    let result = "";
+
+    if (input >= 30) {
+        result += "thirty ";
+        input -= 30;
+    }
+    else if (input >= 20) {
+        result += "twenty ";
+        input = input - 20;
+    }
+
+    if(input in dict)
+        result += dict[input];
+
+    return result;
 }
--- test/index.js
+++ test/index.js
@@ -1,11 +1,35 @@
 import {answer} from '../index';
 import {expect} from "chai";
 
-describe('meaning of life', () => {
+describe('spell out number', () => {
 
-    it('returns 42', () => {
-        const result = answer();
-        expect(result).to.equal(42);
+    it('returns one', () => {
+        const result = answer(1);
+        expect(result).to.equal("one");
     });
 
+    it('returns two', () => {
+        const result = answer(2);
+        expect(result).to.equal("two");
+    });
+
+    it('returns twenty one', () => {
+        const result = answer(21);
+        expect(result).to.equal("twenty one");
+    });
+
+    it('returns twenty three', () => {
+        const result = answer(23);
+        expect(result).to.equal("twenty three");
+    });
+
+    it('returns eleven', () => {
+       const result = answer(11);
+       expect(result).to.equal("eleven");
+    });
+
+    it('returns thirty one', () => {
+        const result = answer(31);
+        expect(result).to.equal("thirty one");
+    });
 });
