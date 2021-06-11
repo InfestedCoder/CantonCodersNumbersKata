88dcee56afe21460dc398d2c7c8816750946b7f6
Jeff Ginn <jeff.ginn@slalom.com>
2021-06-10 21:02:22 -0400

    Changes from Jun 10, 2021 Canton Coders Session

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 export const answer = (input) => {
-    const dict = {1:"one",2:"two",3:"three",11:"eleven"};
+    const dict = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen"
+    };
+
+    if(input===0)
+        return "zero"
+
     let result = "";
 
     if (input >= 30) {
--- test/index.js
+++ test/index.js
@@ -13,6 +13,16 @@ describe('spell out number', () => {
         expect(result).to.equal("two");
     });
 
+    it('returns three', () => {
+        const result = answer(3);
+        expect(result).to.equal("three");
+    });
+
+    it('returns four', () => {
+        const result = answer(4);
+        expect(result).to.equal("four");
+    });
+
     it('returns twenty one', () => {
         const result = answer(21);
         expect(result).to.equal("twenty one");
@@ -31,5 +41,41 @@ describe('spell out number', () => {
     it('returns thirty one', () => {
         const result = answer(31);
         expect(result).to.equal("thirty one");
+    });
+
+    it( 'returns thirty four', () => {
+        const result = answer(34);
+        expect(result).to.equal("thirty four");
+    })
+
+    it( 'returns thirty five', () => {
+        const result = answer(35);
+        expect(result).to.equal("thirty five");
+    })
+    it( 'returns thirty six', () => {
+        const result = answer(36);
+        expect(result).to.equal("thirty six");
+    })
+    it( 'returns thirty seven', () => {
+        const result = answer(37);
+        expect(result).to.equal("thirty seven");
+    })
+    it( 'returns thirty eight', () => {
+        const result = answer(38);
+        expect(result).to.equal("thirty eight");
+    })
+    it( 'returns thirty nine', () => {
+        const result = answer(39);
+        expect(result).to.equal("thirty nine");
+    });
+
+    it( 'returns forty', () => {
+        const result = answer(40);
+        expect(result).to.equal("forty");
+    });
+
+    it( 'returns forty one', () => {
+        const result = answer(41);
+        expect(result).to.equal("forty one");
     });
 });
